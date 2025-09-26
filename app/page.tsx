d79a013beaee1baac521cd56bf3e642c19f72828
amine <amine.bouazzi2021@gmail.com>
2025-09-26 21:03:35 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button className="bg-(--color-yellow-400)"> Click me </Button>
+    </div>
+  );
+};
+
+export default HomePage;
